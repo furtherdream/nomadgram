0a1568be49592b767924eb8fbcc1e9e5494d5d2b
SeungHo Han <furtherdream.ceo@gmail.com>
2019-01-30 12:19:01 +0900

    #1-35 Hidden Model Fields in Django

--- nomadgram/images/serializers.py
+++ nomadgram/images/serializers.py
@@ -4,16 +4,10 @@ from . import models
 
 # 시리얼라이저는 모델과 같은 필드가 있음. 해당 필드만 가져오고 싶다고 입력할 수 있
 # 그 전에 메타클래스를 줘야함 (meta class : Extra Information Class - 설정하는 클래스) 
-class ImageSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = models.Image
-        fields = '__all__'
     
 
 class CommentSerializer(serializers.ModelSerializer):
-
-    image = ImageSerializer()
 
     class Meta:
         model = models.Comment
@@ -22,8 +16,23 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class LikeSerializer(serializers.ModelSerializer):
 
-    image = ImageSerializer()
-
     class Meta:
         model = models.Like
         fields = '__all__'
+
+
+class ImageSerializer(serializers.ModelSerializer):
+
+    comments = CommentSerializer(many=True)
+    likes = LikeSerializer(many=True)
+
+    class Meta:
+        model = models.Image
+        fields = (
+            'id',
+            'file',
+            'location',
+            'caption',
+            'comments',
+            'likes'
+        )
