1a987892536b2e63708c75c62bacddd630c55a59
SeungHo Han <furtherdream.ceo@gmail.com>
2019-01-31 15:55:39 +0900

    #1-49 Following a User

--- nomadgram/users/views.py
+++ nomadgram/users/views.py
@@ -13,3 +13,22 @@ class ExploreUsers(APIView):
         serializer = serializers.ExploreUserSerializer(last_five, many=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class FollowUser(APIView):
+
+    # 나의 유저는 팔로잉하는 유저 리스트가 있다.
+    def post(self, request, id, format=None):
+
+        user = request.user
+
+        try:
+            user_to_follow = models.User.objects.get(id=id)
+        except models.User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        user.following.add(user_to_follow)
+
+        user.save()
+
+        return Response(status=status.HTTP_200_OK)
