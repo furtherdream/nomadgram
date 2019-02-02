963ff2ad45e6097da0c27a3e9b59ba6bd300493d
SeungHo Han <furtherdream.ceo@gmail.com>
2019-02-02 13:39:09 +0900

    #1-56 Searching by Hashtag part Two

--- nomadgram/users/views.py
+++ nomadgram/users/views.py
@@ -99,3 +99,23 @@ class UserFollowing(APIView):
         serializer = serializers.ListUserSerializer(user_following, many=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class Search(APIView):
+
+    def get(self, request, format=None):
+
+        username = request.query_params.get('username', None)
+
+        if username is not None :
+
+            # 스페링까지 정확한 검색을 원하지 않음 : icontains 사용 -> istartswith
+            users = models.User.objects.filter(username__istartswith = username)
+
+            serializer = serializers.ListUserSerializer(users, many=True)
+
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        
+        else : 
+            return Response(status=status.HTTP_400_BAD_REQUEST)
