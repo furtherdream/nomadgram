75129a1724fc010d4cce30415e624fca2c2f2dc1
SeungHo Han <furtherdream.ceo@gmail.com>
2019-02-07 13:31:27 +0900

    #1-70 Setting up JWT

--- config/settings/base.py
+++ config/settings/base.py
@@ -244,3 +244,15 @@ SOCIALACCOUNT_ADAPTER = 'nomadgram.users.adapters.SocialAccountAdapter'
 # Your stuff...
 # ------------------------------------------------------------------------------
 TAGGIT_CASE_INSENSITIVE = True
+
+REST_FRAMEWORK = {
+    # 모든 request 는 승인이 필요하다는 설정
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
--- config/urls.py
+++ config/urls.py
@@ -4,14 +4,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from rest_framework_jwt.views import obtain_jwt_token
+
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/",
-        TemplateView.as_view(template_name="pages/about.html"),
-        name="about",
-    ),
+    # ==> 쿠키커터가 이전에 작성한 내용은 삭제
+    # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    # path(
+    #     "about/",
+    #     TemplateView.as_view(template_name="pages/about.html"),
+    #     name="about",
+    # ), ==> 쿠키커터가 이전에 작성한 내용은 삭제
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
@@ -21,6 +24,7 @@ urlpatterns = [
     ),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("api-token-auth/", obtain_jwt_token),
     path("images/", include("nomadgram.images.urls", namespace="images"),),
     path("notifications/", include("nomadgram.notifications.urls", namespace="notifications"),),
 
